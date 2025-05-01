5e103d5eaeb1e041d4f2bab3a17c038e799c876f
rpalomo123 <122935568+rpalomo123@users.noreply.github.com>
2025-05-01 15:40:48 -0700

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://legendary-liger-663ba1.netlify.app'  // ✅ your frontend domain
+}));
 app.use(express.json());
 
 
