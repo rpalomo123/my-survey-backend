35ea11a482ca6b6f3490884e1bca83d610aebe61
rpalomo123 <122935568+rpalomo123@users.noreply.github.com>
2025-05-01 14:02:30 -0700

    Add files via upload

---
+++ models/Response.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const responseSchema = new mongoose.Schema({
+  surveyId: { type: mongoose.Schema.Types.ObjectId, ref: 'Survey' },
+  answers: [Number],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Response', responseSchema);
